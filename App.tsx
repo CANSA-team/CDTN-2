98889acf788f3eba6527a434720da596281cdd97
VanLoc <locpv.46.student@fit.tdc.edu.vn>
2021-10-01 15:45:21 +0700

    Ngay 1/10/2021

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-
 import SwitchNavigation from './src/components/SwitchNavigation';
+//import OTPscreen from './src/screens/Auth/OTPscreen';
+import ChangePassword from './src/screens/Auth/ChangePassword';
+import Resgister from './src/screens/Auth/Register';
+import Login from './src/screens/Auth/Login';
 
 
 
 export default function App() {
   return (
-      <SwitchNavigation />
+      <Login/>
   );
 }
 
