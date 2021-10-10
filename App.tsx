51bb6b4655f0da62d4db1c43e236a318bd0a9cb1
VanLoc <locpv.46.student@fit.tdc.edu.vn>
2021-10-10 23:08:28 +0700

    Update

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import SwitchNavigation from './src/components/SwitchNavigation';
+import Register from './src/screens/Auth/Register';
 import { Provider } from 'react-redux';
 import { store } from './src/redux/store';
+import PasswordInformation from './src/screens/Auth/PasswordInformation';
 
 export default function App() {
   return ( 
     <Provider store={store}>
-        <SwitchNavigation />     
+        <PasswordInformation/>     
     </Provider>
   );
 }
